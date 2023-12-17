85516c5268694a3c4331d3029a287439ed402670
Kaito Watanabe <marumemomo@gmail.com>
2023-12-17 09:08:44 +0900

    Using simple LLM

--- main.py
+++ main.py
@@ -1,21 +1,64 @@
-from langchain.agents import AgentType, initialize_agent
-from langchain.llms import Ollama
-from langchain.agents.agent_toolkits.github.toolkit import GitHubToolkit
-from langchain.utilities.github import GitHubAPIWrapper
-from dotenv import load_dotenv
+import yaml
+import re
+from langchain.llms.ollama import Ollama
+from github import Auth, GithubIntegration
+import tomllib
+import dotenv
+import os
 
-load_dotenv()
+dotenv.load_dotenv()
+app_id = os.getenv("GITHUB_APP_ID")
+with open(os.getenv("GITHUB_APP_PRIVATE_KEY"), "r") as f:
+    private_key = f.read()
+auth = Auth.AppAuth(
+    app_id=app_id,
+    private_key=private_key,
+)
+gi = GithubIntegration(auth=auth)
+installation = gi.get_installations()[0]
+g = installation.get_github_for_installation()
 
+github_issue_list = []
+
+for issue in g.get_repo("marumemomo/MushockAGI").get_issues(state='open'):
+    toml_data = tomllib.loads(issue.body)
+    print(toml_data)
+    github_issue_list.append({
+        'description': toml_data["description"],
+        'file_path': toml_data['file_path'],
+    })
+
+with open('config.yml', 'r') as stream:
+    config = yaml.safe_load(stream)
+
+model = config['model']
 llm = Ollama(
-    model="llama2:13b",
+    model=model
 )
 
-github = GitHubAPIWrapper()
-toolkit = GitHubToolkit.from_github_api_wrapper(github)
-tools = toolkit.get_tools()
+for task in github_issue_list:
+    with open(task['file_path'], 'r') as code_file:
+        code = code_file.read()
+    print("[Invoke LLM]")
+    response =  llm.invoke(
+    f"""
+    You are a Professional Python programmer.
+    Please change the following code with the following description.
+    The changed code should be output using code blocks.
 
-agent_executor = initialize_agent(
-    tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
-)
+    Description:
+    {task['description']}
 
-agent_executor.run("Tell me about marumemomo/MushockAGI github repo")
+    Code:
+    {code}
+    """)
+
+    # Extracting code block from response
+    matches = re.findall(r'```.*\n([\s\S]*?)\n```', response, re.MULTILINE)
+    if len(matches) > 0:
+        changed_code = matches[0]
+    else:
+        print("No code block found in the response.")
+        changed_code = response
+    print("[Code Result]")
+    print(changed_code)
