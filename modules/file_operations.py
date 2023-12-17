49056d8615210ab6d02d17136f54faccea63b918
Kaito Watanabe <marumemomo@gmail.com>
2023-12-17 14:01:00 +0900

    Automated update for issue #13

--- modules/file_operations.py
+++ modules/file_operations.py
@@ -1,4 +1,6 @@
 import subprocess
+import os
+from config_loader import load_config
 
 def read_file(file_path):
     with open(file_path, 'r') as file:
@@ -10,4 +12,8 @@ def write_file(file_path, content):
 
 def run_git_commands(commands):
     for command in commands:
+        cwd = os.getcwd()  # Get current working directory
+        root_dir = load_config()['root_directory']  # Load project root directory from config
+        if cwd != root_dir:
+            os.chdir(root_dir)  # Change to the project root directory
         subprocess.run(command)
