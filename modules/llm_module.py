5767e88e6de41e91a00037e89c9aea8d6706907e
Kaito Watanabe <marumemomo@gmail.com>
2023-12-17 13:45:32 +0900

    Automated update for issue #11

--- modules/llm_module.py
+++ modules/llm_module.py
@@ -1,11 +1,11 @@
-import re
+ import re
 
 def invoke_llm(llm, task):
     response = llm.invoke(
         f"""
         You are a Professional Python programmer.
         Please change the following code with the following description.
-        The changed code should be output using code blocks.
+        The changed code should be output using streaming mode.
         Be sure to output all code for final export to a file.
 
         Description:
@@ -13,7 +13,7 @@ def invoke_llm(llm, task):
 
         Code:
         {task['code']}
-        """)
+        """, stream_response=True)
     # Extracting code block from response
     matches = re.findall(r'```.*\n\s*([\s\S]*?)\n```', response, re.MULTILINE)
     if len(matches) > 0:
